417e88e40ea80db23f99932d5dc4d2c66ec4a0cf
leechenze <leeczyc@gmail.com>
2023-07-08 19:17:40 +0800

    P005T000000

--- GoWeb/GoTemplate/main.go
+++ GoWeb/GoTemplate/main.go
@@ -1,5 +1,34 @@
 package GoTemplate
 
-func Main() {
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	// 解析模版
+	template, err := template.ParseFiles("./GoTemplate/hello.tmpl")
+	if err != nil {
+		fmt.Printf("parse template failed: %v\n", err)
+		return
+	}
+	// 渲染模版
+	data := "模版内容"
+	err = template.Execute(w, data)
+	if err != nil {
+		fmt.Printf("render template failed: %v\n", err)
+		return
+	}
+}
+
+func Main() {
+	http.HandleFunc("/", sayHello)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		fmt.Printf("HTTP server start failed: %v\n", err)
+		return
+	} else {
+		fmt.Println("9000 serve is running")
+	}
 }
